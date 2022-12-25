bf9937cd1b061ba924d0d477f9ecefd9f8d85176
Drctn <53501900+Drctn@users.noreply.github.com>
2022-12-25 17:48:00 -0500

    Add files via upload

---
+++ FirstAssignment.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
+
+contract Task01 {
+
+    string public textVar = "Reply";
+    int8 public varIntA = -48;
+    int16 public varIntB = 26322;
+    uint32 public varIntC = 4673236;
+    uint64 public varIntD = 3248556999457;
+    bool public option0 = false;
+    address public owner = 0xf7aca059208402B13c836cdC13cb2b4cc40472a5;
+    
+    bytes1 i = 0x43;
+    
+    enum trafficLight {Red, Amber, Green}
+      
+
+}
